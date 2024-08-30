fa4eead8e1adfbeecba03c63edfed6d013cba125
ana <ana@DESKTOP-NT2O60E>
2024-08-30 07:16:25 +0200

    Add project files.

---
+++ assiment 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace assiment_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ html_css.lecture2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace html_css.lecture2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
